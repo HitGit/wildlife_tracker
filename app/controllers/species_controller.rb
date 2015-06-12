9077a6366cbbeae84e187b754118ad68959ee72a
Ida Woo <ida@Idas-MacBook-Pro.local>
2015-06-12 16:46:08 -0700

    species model

---
+++ app/controllers/species_controller.rb
@@ -0,0 +1,55 @@
+class SpeciesController < ApplicationController
+
+  def index
+    @allspecies = Species.all
+    @species = Species.new
+    render('species/index.html.erb')
+
+
+  end
+
+  def show
+    @species = Species.find(params[:id])
+    render('species/show.html.erb')
+  end
+
+  def new
+    @species = Species.new
+    render('species/new.html.erb')
+  end
+
+  def create
+    @species = Species.new(:name => params[:name])
+    if @species.save
+      render('species/success.html.erb')
+    else
+      render('species/new.html.erb')
+    end
+  end
+
+
+  def edit
+    @species = Species.find(params[:id])
+    render('species/edit.html.erb')
+  end
+
+
+  def update
+    @species = Species.find(params[:id])
+    if @species.update(:name => params[:name])
+      render('species/success.html.erb')
+    else
+      render('species/edit.html.erb')
+    end
+  end
+
+
+  def destroy
+    @species = Species.find(params[:id])
+    @species.destroy
+    render('species/destroy.html.erb')
+    end
+
+
+
+end
